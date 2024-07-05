bf19b9ca3dd6d74512c1356537c1eceb020aa1a9
Hyeri Jeong <cos850@naver.com>
2024-07-06 01:39:46 +0900

    1978 all 과 컴프리헨션을 통한 축약

--- sec02_basic/2_최적화기법_정수론/1978.py
+++ sec02_basic/2_최적화기법_정수론/1978.py
@@ -1,17 +1,8 @@
 input()
-numbers = set(map(int, input().split()))
 
 def isPn(n) :
-    if n < 2 : 
-        return False
-    for i in range(2, int(n**0.5)+1) :
-        if n % i == 0 :
-            return False
-    return True
+    n = int(n)
+    if n < 2 : return 0
+    return 1 if all(n % i for i in range(2, int(n**0.5)+1)) else 0
 
-r = 0
-for n in numbers :
-    if isPn(n) : 
-        r += 1
-
-print(r)
+print(sum(map(isPn, input().split())))
